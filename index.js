6371099cb5a927829c8f830791871ae3e71b86c3
caJoey <71573782+caJoey@users.noreply.github.com>
2023-03-24 00:01:28 -0700

    Fully working going forward only

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ function getValue(){
 // initializations
 let line = 0;
 let finalSeatLine = 2;
+let finalAnteLine = 0;
+let holeLine = 0;
+let showDownLine = 0;
 let arr = [];
 let arrTemp = [];
 let totalPot = 0;
+let rake = 0;
 
 // if not null: 
 // 0-1: players name (string), players balance (float)
@@ -51,9 +55,16 @@ function setArr(){
     let hand = sessionStorage.getItem("hand");
     arrTemp = hand.split('\n');
     for(let i = 0; i < arrTemp.length; i++){
+        if(arrTemp[i].includes("***") && arrTemp[i].includes("SUMMARY")) break;
         arr.push(arrTemp[i].split(' '));
     }
+
+    findRake();
     setFinalSeatLine();
+    finalAnteLine = finalSeatLine;
+    if(arr[finalSeatLine + 1][3] == "ante"){
+        finalAnteLine = finalSeatLine * 2 - 1;
+    }
     right();
 
     // set attributes of box and name
@@ -123,11 +134,25 @@ function setFinalSeatLine(){
         finalSeatLine++;
     }
     finalSeatLine--;
+
+    while(arr[holeLine][1] != "HOLE"){
+        holeLine++;
+    }
+    holeLine++;
 }
 
 // "next" button is clicked
 function right(){
-    if(line >= arr.length) return;
+    if(line >= arr.length){
+        for(let i = 0; i < 9; i++){
+            if(document.getElementById("bet" + i).innerHTML.length > 0){
+                let total = parseFloat(document.getElementById("bet" + i).innerHTML);
+                document.getElementById("bet" + i).innerHTML = "";
+                updateBalance(0 - total, i);
+                break;
+            }
+        }
+    }
 
     if(line <= 1){
         let s = "";
@@ -159,7 +184,7 @@ function right(){
             let bal = arr[i][3].slice(1,arr[i][3].length);
 
             let num = parseInt(s.slice(0, s.length - 1)) - 1;
-            pairs[parseInt(num)] = [player];
+            pairs[num] = [player];
 
             // name appears
             document.getElementById("name" + num).innerHTML = player;
@@ -168,29 +193,30 @@ function right(){
             document.getElementById("box" + num).style.display = "block";
             
             // adds balance to pairs hashtable
-            pairs[parseInt(num)].push(parseFloat(bal));
+            pairs[num].push(removeComma(bal));
 
             // balance appears
-            document.getElementById("bal" + num).innerHTML = bal;
+            document.getElementById("bal" + num).innerHTML = removeComma(bal);
             document.getElementById("bal" + num).style.display = "block";
         }
         line = finalSeatLine + 1;
     }
-    else if(line == finalSeatLine + 1){
-        totalPot += parseFloat(arr[line][4]);
+    // post small / big blind or ante
+    else if(arr[line][1] == "posts"){
+        if(arr[line][2] == "small"){
+            collectBets();
+        }
+        totalPot += parseFloat(removeComma(arr[line][4]));
         let numValue = getPlayerNumValue(arr[line][0].slice(0,arr[line][0].length - 1));
-        document.getElementById("bet" + numValue).innerHTML = arr[line][4];
-        line++;
-    }
-    else if(line == finalSeatLine + 2){
-        totalPot += parseFloat(arr[line][4]);
-        let numValue = getPlayerNumValue(arr[line][0].slice(0,arr[line][0].length - 1));
-        document.getElementById("bet" + numValue).innerHTML = arr[line][4];
+        // updates the players balance
+        updateBalance(parseFloat(arr[line][4]), numValue);
+        //update bet value
+        document.getElementById("bet" + numValue).innerHTML = removeComma(arr[line][4]);
         line++;
     }
     // hole cards
-    else if(line <= finalSeatLine + 4){
-        line = finalSeatLine + 4;
+    else if(line <= holeLine){
+        line = holeLine;
         let firstCard = arr[line][3].slice(1,arr[line][3].length);
         let secondCard = arr[line][4].slice(0,arr[line][3].length - 1);
         for(let i = 0; i < 9; i++){
@@ -203,17 +229,195 @@ function right(){
             document.getElementById("card" + i + 0).style.display = "block";
             document.getElementById("card" + i + 1).style.display = "block";
         }
+        let tempLine = line;
+        // sets finalHoleCardsLine to flop line (*** FLOP ***)
+        while(arr[tempLine][1] != "SHOW"){
+            tempLine++;
+            showDownLine = tempLine;
+        }
+        line++;
+    }
+
+    // (*** FLOP ***), collects bets, does flop
+    else if(arr[line][1] == "FLOP"){
+        collectBets();
+        let threeArr = [arr[line][3].slice(1),
+        arr[line][4],
+        arr[line][5].slice(0, arr[line][5].length - 1)];
+        for(let i = 0; i < 3; i++){
+            document.getElementById("flop" + i).src = "../images/cards/" + threeArr[i] + ".jpg";
+            document.getElementById("flop" + i).style.display = "block";
+        }
+        line++;
+    }
+
+    // (*** TURN ***)
+    else if(arr[line][1] == "TURN"){
+        collectBets();
+        let turnCard = arr[line][6].slice(1,arr[line][6].length - 1);
+        document.getElementById("turn").src = "../images/cards/" + turnCard + ".jpg";
+        document.getElementById("turn").style.display = "block";
+        line++;
+    }
+
+    // (*** RIVER ***)
+    else if(arr[line][1] == "RIVER"){
+        collectBets();
+        let riverCard = arr[line][7].slice(1,arr[line][7].length - 1);
+        document.getElementById("river").src = "../images/cards/" + riverCard + ".jpg";
+        document.getElementById("river").style.display = "block";
+        line++;
+    }
+
+    // (*** SHOW DOWN ***)
+    else if(arr[line][1] == "SHOW"){
+        collectBets();
+        line++;
+        right();
+        return;
+    }
+
+    // from second line of hole cards to line before (*** SHOW DOWN ***)
+    else if(line < showDownLine){
+        let identifier = arr[line][1];
+        let name = arr[line][0].slice(0, arr[line][0].length - 1);
+        let num = getPlayerNumValue(name);
+        if(identifier == "folds"){
+            document.getElementById("box" + num).style.backgroundColor = "gray";
+            document.getElementById("card" + num + 0).style.display = "none";
+            document.getElementById("card" + num + 1).style.display = "none";
+        }
+        if(identifier == "raises" || identifier == "bets"){
+            document.getElementById("box" + num).style.backgroundColor = "lightcoral";
+            let raiseAmount = parseFloat(removeComma(arr[line][2]));
+            totalPot += raiseAmount;
+            updateBalance(raiseAmount, num);
+            if(identifier == "raises"){
+                document.getElementById("bet" + num).innerHTML = removeComma(arr[line][4]);
+            }
+            if(identifier == "bets"){
+                document.getElementById("bet" + num).innerHTML = removeComma(arr[line][2]);
+            }
+        }
+        // calls (difference between the opponents bet and their current bet)
+        if(identifier == "calls"){
+            document.getElementById("box" + num).style.backgroundColor = "lightsalmon";
+            let callAmount = parseFloat(removeComma(arr[line][2]));
+            totalPot += callAmount
+            updateBalance(callAmount, num);
+            let firstAdd = document.getElementById("bet" + num).innerHTML;
+            if(firstAdd.length == 0){
+                firstAdd = "0";
+            }
+            let totalAdd = parseFloat(firstAdd) + parseFloat(callAmount);
+            document.getElementById("bet" + num).innerHTML = totalAdd.toString();
+        }
+        // checks, just changes box color
+        if(identifier == "checks"){
+            document.getElementById("box" + num).style.backgroundColor = "lightgreen";
+        }
+        // uncalled bet returned
+        if(identifier == "bet"){
+            name = arr[line][5];
+            num = getPlayerNumValue(name);
+            let betReverse = 0 - parseFloat(removeComma(arr[line][2].slice(1, arr[line][2].length - 1)));
+            updateBalance(betReverse, num);
+
+            // update totalPot
+            totalPot += parseFloat(betReverse.toFixed(2));
+            document.getElementById("pot").innerHTML = "Total Pot: " + totalPot.toString();
+
+            line++;
+            right();
+            return;
+        }
+        line++;
+    }
+    // from line after (*** SHOW DOWN ***) to final line
+    else if(line <= arr.length - 1){
+        let player = arr[line][0].replace(':', '');
+        let playerNum = getPlayerNumValue(player);
+        if(arr[line][1] == "shows"){
+            let firstShow = arr[line][2].slice(1, arr[line][2].length);
+            let secondShow = arr[line][3].slice(0, arr[line][3].length - 1);
+            document.getElementById("card" + playerNum + 0).src = "../images/cards/" + firstShow + ".jpg";
+            document.getElementById("card" + playerNum + 1).src = "../images/cards/" + secondShow + ".jpg";
+        }
+        else if(arr[line][1] == "mucks"){
+            document.getElementById("box" + playerNum).style.backgroundColor = "gray";
+            document.getElementById("card" + playerNum + 0).style.display = "none";
+            document.getElementById("card" + playerNum + 1).style.display = "none";
+        }
+        else if(arr[line][1] == "collected"){
+            let potUpdated = arr[line][2];
+            document.getElementById("bet" + playerNum).innerHTML = removeComma(potUpdated);
+            document.getElementById("rake").innerHTML = "Rake: " + rake.toString();
+            document.getElementById("rake").style.display = "block";
+        }
+        else{
+            line++;
+            right();
+            return;
+        }
+        line++;
     }
 }
 
 // returns seat number (0-8) given player name
 function getPlayerNumValue(playerName){
     for(let i = 0; i < 9; i++){
+        if(pairs[i] == null){
+            continue;
+        }
         if(pairs[i][0] == playerName){
             return i;
         }
     }
 }
+
+// updates player's balance given the bet size (float) and their player number (int)
+function updateBalance(betSize, playerNumValue){
+    betSize = betSize.toFixed(2);
+    pairs[playerNumValue][1] -= betSize;
+    document.getElementById("bal" + playerNumValue).innerHTML = parseFloat(pairs[playerNumValue][1]).toFixed(2);
+}
+
+// removes commas from string s
+function removeComma(s){
+    let returnString = ""
+    for(let i = 0; i < s.length; i++){
+        if(s[i] != ','){
+            returnString += s[i];
+        }
+    }
+    return returnString;
+}
+// hides all bets, reveals new totalPot value
+// also resets colors of non-folded hands to light blue
+function collectBets(){
+    document.getElementById("pot").style.display = "block";
+    document.getElementById("pot").innerHTML = "Total Pot: " +
+    (Math.round((totalPot + Number.EPSILON) * 100) / 100).toString();
+    for(let i = 0; i < 9; i++){
+        document.getElementById("bet" + i).innerHTML = "";
+        if(document.getElementById("box" + i).style.backgroundColor != "gray"){
+            document.getElementById("box" + i).style.backgroundColor = "lightblue";
+        }
+    }
+}
+
+// finds rake value
+function findRake(){
+    for(let i = 0; i < arrTemp.length; i++){
+        if(arrTemp[i].includes("Rake")){
+            let rakeArr = arrTemp[i].split(' ');
+            rake = parseFloat(rakeArr[5]);
+            return;
+        }
+    }
+}
+
+// prev button pushed
 function left(){
-    alert(pairs[1][0].toString() + pairs[1][1]);
+    alert("under construction");
 }
